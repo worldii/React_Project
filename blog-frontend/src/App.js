113950dbb87eec85a38786a77f35cfff2d918e4d
worldi <jongha2788@u.sogang.ac.kr>
2021-09-24 17:50:39 +0900

    초기환경 세팅

---
+++ blog-frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import LoginPage from './pages/LoginPage';
+import PostPage from './pages/PostPage';
+import RegisterPage  from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <Route component={PostListPage} path={['/@:username','/']} exact/>
+      <Route component={LoginPage} path="/login"/> 
+      <Route component={RegisterPage} path="/register"/>
+      <Route component={WritePage} path="/write"/>
+      <Route component={PostPage} path="/@:username/:postId"/> 
+    
+    </>
+  );
+
+}
+
+export default App;
---
+++ blog-frontend/src/pages/RegisterPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const RegisterPage = () => {
+    return <div>회원 가입</div>;
+}
+export default RegisterPage;
